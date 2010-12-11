6ba89e67a478276d763fba1d4fcbd5189ee0a233
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-12-10 16:21:55 -0800

    Added stub module prefix.

--- marrow/wsgi/exceptions/__init__.py
+++ marrow/wsgi/exceptions/__init__.py
@@ -1,4 +1,10 @@
-try: # pragma: no cover
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError: # pragma: no cover
-    __import__('pkgutil').extend_path(__path__, __name__)
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+
+log = __import__('logging').getLogger(__name__)
+__all__ = []
+
+
+
