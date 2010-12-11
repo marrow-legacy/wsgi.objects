f8d7b26326cf29e5556831a853229161401e7b10
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-12-10 16:14:57 -0800

    Initial commit of project stub.

---
+++ marrow/wsgi/exceptions/release.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+"""Release information about Marrow Script."""
+
+
+name = "marrow.wsgi.exceptions"
+version = "0.1"
+release = "0.1"
+
+summary = "Exceptions representing HTTP responses for specific status codes."
+description = """For full documentation, see the README.textile file present in the package,
+or view it online on the GitHub project page:
+
+https://github.com/pulp/marrow.wsgi.exceptions"""
+author = "Alice Bevan-McGregor"
+email = "alice@gothcandy.com"
+url = "https://github.com/pulp/marrow.wsgi.exceptions"
+download_url = "https://github.com/pulp/marrow.wsgi.exceptions"
+copyright = "2010, Alice Bevan-McGregor and contributors"
+license = "MIT"
---
+++ setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys, os
+
+try:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+
+except ImportError:
+    pass
+
+from setuptools import setup, find_packages
+
+
+if sys.version_info <= (2, 6):
+    raise SystemExit("Python 2.6 or later is required.")
+
+if sys.version_info >= (3,0):
+    def execfile(filename, globals_=None, locals_=None):
+        if globals_ is None:
+            globals_ = globals()
+        
+        if locals_ is None:
+            locals_ = globals_
+        
+        exec(compile(open(filename).read(), filename, 'exec'), globals_, locals_)
+
+else:
+    from __builtin__ import execfile
+
+execfile(os.path.join("marrow", "wsgi", "exceptions", "release.py"), globals(), locals())
+
+
+
+setup(
+        name = name,
+        version = version,
+        
+        description = summary,
+        long_description = description,
+        author = author,
+        author_email = email,
+        url = url,
+        download_url = download_url,
+        license = license,
+        keywords = '',
+        
+        use_2to3 = True,
+        install_requires = ['marrow.util'],
+        
+        test_suite = 'nose.collector',
+        tests_require = [
+                'nose',
+                'coverage'
+            ],
+        
+        classifiers = [
+                "Development Status :: 5 - Production/Stable",
+                "Environment :: Console",
+                "Intended Audience :: Developers",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+                "Programming Language :: Python",
+                "Programming Language :: Python :: 2.6",
+                "Programming Language :: Python :: 2.7",
+                "Programming Language :: Python :: 3",
+                "Programming Language :: Python :: 3.1",
+                "Programming Language :: Python :: 3.2",
+                "Topic :: Software Development :: Libraries :: Python Modules",
+                "Topic :: Utilities"
+            ],
+        
+        packages = find_packages(exclude=['examples', 'tests', 'tests.*', 'docs', 'third-party']),
+        include_package_data = True,
+        package_data = {
+                '': ['README.textile', 'LICENSE', 'distribute_setup.py'],
+                'docs': ['Makefile', 'source/*']
+            },
+        zip_safe = True,
+        
+        namespace_packages = ['marrow'],
+        
+        entry_points = {}
+    )
