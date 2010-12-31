2ae44b24e0cf437df2e7890867db22608acf23d9
Alex Grönholm <alex.gronholm+git@nextday.fi>
2010-12-31 04:59:05 +0200

    Major cleanup of setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-# encoding: utf-8
+import sys
+import os
 
-import sys, os
-
-try:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-
-except ImportError:
-    pass
+from distribute_setup import use_setuptools
+use_setuptools()
 
 from setuptools import setup, find_packages
 
@@ -16,70 +11,38 @@ from setuptools import setup, find_packages
 if sys.version_info <= (2, 6):
     raise SystemExit("Python 2.6 or later is required.")
 
-if sys.version_info >= (3,0):
-    def execfile(filename, globals_=None, locals_=None):
-        if globals_ is None:
-            globals_ = globals()
-        
-        if locals_ is None:
-            locals_ = globals_
-        
-        exec(compile(open(filename).read(), filename, 'exec'), globals_, locals_)
-
-else:
-    from __builtin__ import execfile
-
-execfile(os.path.join("marrow", "wsgi", "objects_release.py"), globals(), locals())
-
-
+exec(open(os.path.join("marrow", "wsgi", "objects", "release.py")))
 
 setup(
-        name = name,
-        version = version,
-        
-        description = summary,
-        long_description = description,
-        author = author,
-        author_email = email,
-        url = url,
-        download_url = download_url,
-        license = license,
-        keywords = '',
-        
-        use_2to3 = True,
-        install_requires = ['marrow.util'],
-        
-        test_suite = 'nose.collector',
-        tests_require = [
-                'nose',
-                'coverage'
-            ],
-        
-        classifiers = [
-                "Development Status :: 5 - Production/Stable",
-                "Environment :: Console",
-                "Intended Audience :: Developers",
-                "License :: OSI Approved :: MIT License",
-                "Operating System :: OS Independent",
-                "Programming Language :: Python",
-                "Programming Language :: Python :: 2.6",
-                "Programming Language :: Python :: 2.7",
-                "Programming Language :: Python :: 3",
-                "Programming Language :: Python :: 3.1",
-                "Programming Language :: Python :: 3.2",
-                "Topic :: Software Development :: Libraries :: Python Modules",
-                "Topic :: Utilities"
-            ],
-        
-        packages = find_packages(exclude=['examples', 'tests', 'tests.*', 'docs', 'third-party']),
-        include_package_data = True,
-        package_data = {
-                '': ['README.textile', 'LICENSE', 'distribute_setup.py'],
-                'docs': ['Makefile', 'source/*']
-            },
-        zip_safe = True,
-        
-        namespace_packages = ['marrow'],
-        
-        entry_points = {}
-    )
+    name='marrow.wsgi.objects',
+    version=release,
+    description='A collection of object-oriented WSGI2 helpers.',
+    long_description='''\
+For full documentation, see the README.textile file present in the package,
+or view it online on the GitHub project page:
+
+https://github.com/pulp/marrow.wsgi.objects''',
+    author='Alice Bevan-McGregor',
+    author_email='alice@gothcandy.com',
+    url='https://github.com/pulp/marrow.wsgi.objects',
+    license='MIT',
+    install_requires=['marrow.util'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(exclude=['examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+)
