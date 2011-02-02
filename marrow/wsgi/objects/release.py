b2e61cd352ad7e347ca5e8d652a0e5d289af3804
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-02-01 17:33:54 -0800

    Some minor organizational modifications, utilization of named tuples for version_info information, and utilization of the first letter of the 'releaselevel' (e.g. alpha) for non-final releases when constructing the release string.

--- marrow/wsgi/objects/release.py
+++ marrow/wsgi/objects/release.py
@@ -2,11 +2,14 @@
 
 """Release information about Marrow WSGI Objects."""
 
+from collections import namedtuple
+
 
 __all__ = ['version_info', 'version', 'release']
 
 
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))\
+        (0, 1, 0, 'alpha', 1)
 
-version_info = (0, 1, 0, 'alpha')
-version = '.'.join(str(n) for n in version_info[:3])
-release = version + ''.join(str(n) for n in version_info[3:])
+version = '.'.join(str(i) for i in version_info[:3])
+release = version + (version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else ''
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-import sys
-import os
+# encoding: utf-8
 
-from distribute_setup import use_setuptools
-use_setuptools()
+import os
+import sys
+
+__import__('distribute_setup').use_setuptools()
 
 from setuptools import setup, find_packages
 
@@ -13,36 +14,44 @@ if sys.version_info <= (2, 6):
 
 exec(open(os.path.join("marrow", "wsgi", "objects", "release.py")))
 
+
 setup(
-    name='marrow.wsgi.objects',
-    version=release,
-    description='A collection of object-oriented WSGI2 helpers.',
-    long_description='''\
+        name = 'marrow.wsgi.objects',
+        version = release,
+        
+        description = 'A collection of object-oriented WSGI2 helpers.',
+        long_description = '''\
 For full documentation, see the README.textile file present in the package,
 or view it online on the GitHub project page:
 
 https://github.com/pulp/marrow.wsgi.objects''',
-    author='Alice Bevan-McGregor',
-    author_email='alice@gothcandy.com',
-    url='https://github.com/pulp/marrow.wsgi.objects',
-    license='MIT',
-    install_requires=['marrow.util'],
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    packages=find_packages(exclude=['examples', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
-)
+        
+        author = 'Alice Bevan-McGregor',
+        author_email = 'alice@gothcandy.com',
+        url = 'https://github.com/pulp/marrow.wsgi.objects',
+        license = 'MIT',
+        
+        install_requires = ['marrow.util'],
+        test_suite = 'nose.collector',
+        tests_require = ['nose'],
+        
+        classifiers = [
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.1",
+            "Programming Language :: Python :: 3.2",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+        
+        packages = find_packages(exclude=['examples', 'tests']),
+        include_package_data = True,
+        zip_safe = True,
+        
+        namespace_packages = ['marrow']
+    )
